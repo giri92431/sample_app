eff3dac34271e2f16bdb3f7be3a474734192255c
girish <girish@ubuntu.(none)>
2013-11-05 12:19:41 +0530

    new name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+#return  atitle on a page
+def title 
+ base_title ="ruby on rails tutorial sample app"
+ if @title.nil?
+ base_title
+ else
+ "#{base_title} | #{@title}"
+ end
+end
 end
