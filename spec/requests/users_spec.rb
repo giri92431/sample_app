e94a307a7ddacbb45de752a25d736a2114b2f12a
girish <girish@ubuntu.(none)>
2013-11-12 00:46:12 +0530

    sign up page

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe "Users" do
+  
+   describe "signup" do
+     
+ 
+       describe "failure" do
+         lambda do 
+         it "should noy make a new user"do
+          visit signup_path
+          fill_in "name",          :with=>""
+          fill_in "email",         :with=>""
+          fill_in "password",      :with=>""
+          fill_in "Confirmation",  :with=>""
+          click_button
+          response.should render_template('users/new')
+          response.should have_selector('div#error_explanation')
+         end.should_not change(User,:count)
+       end
+           
+      end
+#------------------------------------------------------------------------
+
+
+       describe "success" do
+      it "should make a new user" do
+        lambda do
+          visit signup_path
+          fill_in "Name",         :with => "Example User"
+          fill_in "Email",        :with => "user@example.com"
+          fill_in "Password",     :with => "foobar"
+          fill_in "Confirmation", :with => "foobar"
+          click_button
+          response.should have_selector('div.flash.success',
+                                        :content => "Welcome")
+          response.should render_template('users/show')
+        end.should change(User, :count).by(1)
+          
+          
+        end  
+   
+       end
+
+#----------------------------------------------------------------------------
+    end
+    
+end
