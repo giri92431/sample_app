1b2437e909286b4b39822abdd90a01d8f235dfba
girish <girish@ubuntu.(none)>
2013-11-12 15:24:17 +0530

    sign in form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,21 @@ class SessionsController < ApplicationController
   def new
   @title="sign in"
   end
-   
+  #------------------------- 
   def create
-   end
-
+   user = User.authenticate(params[:session][:email],
+                            params[:session][:password])
+    if user.nil?
+     @title="sign in"
+     flash.now[:error]="invalid email/password combination"
+     render 'new'
+    
+    else
+    
+    end
+   
+  end
+#-----------------
    def destory
    end  
 
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe SessionsController do
 render_views
+  
+#-----------------------------------
   describe "GET 'new'" do
     it "returns http success" do
       get :new
@@ -13,9 +15,34 @@ render_views
      get :new
     response.should have_selector('title',:content =>"sign in")
    end
-
-
-
   end
+#------------------------------------------
+describe"post create"do
+  
+   describe "failure"do
+  
+     before(:each)do
+      @attr={:email=>"",:password=>""}
+     end 
+  
+     it"should re-render the new page"do
+     post :create,:session=>@attr
+    response.should render_template('new')
+    end
+
+       it"should have a rit title"do
+       post :create,:session=>@attr
+       response.should have_selector('title',:content=>"sign in")
+       end    
+
+
+     it"should have a error msg"do
+      post :create,:session =>@attr
+     flash.now[:error].should =~/invalid/i
+     end
+ 
+end
+end
+
 
 end
