358ceb69efe8fbc6e1e3ad29e6069f9617cd76fe
girish <girish@ubuntu.(none)>
2013-11-17 12:23:40 +0530

    showing microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,12 @@ class UsersController < ApplicationController
   before_filter :authenticate,:only=>[:index,:edit,:update,:destroy]
   before_filter :correct_user,:only=>[:edit,:update]
   before_filter :admin_user,  :only=> :destroy
-  def show 
+ 
+
+
+ def show 
   @user =User.find(params[:id])
+ @microposts = @user.microposts.paginate(page: params[:page])
   @title=@user.name
   end
  
