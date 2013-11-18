86441c47be3d322d9a0ff90945d999aef4ee7278
girish <girish@ubuntu.(none)>
2013-11-18 19:14:34 +0530

    user relationship interface

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -136,8 +136,20 @@ it"should display micropost count "do
                                 :content=>@user.microposts.count.to_s)
 end
 
+ #---
+  describe "when signed in as another user"do
+   it"should be success ful"do
+     test_sign_in(Factory(:user,:email=>Factory.next(:email)))
+     get :show,:id=>@user
+     response.should be_success
+   end
+ 
 
 
+
+
+  end
+ #------- 
 end
 #---------------------------------------
   describe "GET 'new'" do
